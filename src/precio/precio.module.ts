06d379b551a21dd00a7bb93516718c91863727f7
alejandraxxxx <xagacia.03@gmail.com>
2025-03-20 10:51:55 -0600

    CRUD precio

--- src/precio/precio.module.ts
+++ src/precio/precio.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrecioService } from './precio.service';
 import { PrecioController } from './precio.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Precio } from './entities/precio.entity';
 
 @Module({
   controllers: [PrecioController],
   providers: [PrecioService],
+  imports: [TypeOrmModule.forFeature([Precio])],
+  exports: [TypeOrmModule]
 })
 export class PrecioModule {}
